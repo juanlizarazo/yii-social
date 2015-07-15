148c7cc8337bdff4c63e6bd16f1cdee25317a9ae
Juan Lizarazo <gabriellg4@gmail.com>
2015-07-15 00:24:44 -0600

    Remove tag table from migration. Add down drop table statements

--- app/protected/migrations/m150715_054411_initial_migration.php
+++ app/protected/migrations/m150715_054411_initial_migration.php
@@ -5,6 +5,7 @@ class m150715_054411_initial_migration extends CDbMigration
 	public function up()
 	{
         // Original blog schema sql turned into a migration
+        // TODO: Turn into standard yii migration statements
         $sql = <<<'SQL'
             CREATE TABLE lookup
             (
@@ -29,7 +30,6 @@ class m150715_054411_initial_migration extends CDbMigration
                 id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
                 title VARCHAR(128) NOT NULL,
                 content TEXT NOT NULL,
-                tags TEXT,
                 status INTEGER NOT NULL,
                 create_time INTEGER,
                 update_time INTEGER,
@@ -52,13 +52,6 @@ class m150715_054411_initial_migration extends CDbMigration
                     REFERENCES post (id) ON DELETE CASCADE ON UPDATE RESTRICT
             ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
-            CREATE TABLE tag
-            (
-                id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
-                name VARCHAR(128) NOT NULL,
-                frequency INTEGER DEFAULT 1
-            ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
             INSERT INTO lookup (name, type, code, position) VALUES ('Draft', 'PostStatus', 1, 1);
             INSERT INTO lookup (name, type, code, position) VALUES ('Published', 'PostStatus', 2, 2);
             INSERT INTO lookup (name, type, code, position) VALUES ('Archived', 'PostStatus', 3, 3);
@@ -66,16 +59,12 @@ class m150715_054411_initial_migration extends CDbMigration
             INSERT INTO lookup (name, type, code, position) VALUES ('Approved', 'CommentStatus', 2, 2);
 
             INSERT INTO user (username, password, email) VALUES ('demo','$2a$10$JTJf6/XqC94rrOtzuF397OHa4mbmZrVTBOQCmYD9U.obZRUut4BoC','webmaster@example.com');
-            INSERT INTO post (title, content, status, create_time, update_time, author_id, tags) VALUES ('Welcome!','This blog system is developed using Yii. It is meant to demonstrate how to use Yii to build a complete real-world application. Complete source code may be found in the Yii releases.
+            INSERT INTO post (title, content, status, create_time, update_time, author_id) VALUES ('Welcome!','This blog system is developed using Yii. It is meant to demonstrate how to use Yii to build a complete real-world application. Complete source code may be found in the Yii releases.
 
-            Feel free to try this system by writing new posts and posting comments.',2,1230952187,1230952187,1,'yii, blog');
-            INSERT INTO post (title, content, status, create_time, update_time, author_id, tags) VALUES ('A Test Post', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', 2,1230952187,1230952187,1,'test');
+            Feel free to try this system by writing new posts and posting comments.',2,1230952187,1230952187,1);
+            INSERT INTO post (title, content, status, create_time, update_time, author_id) VALUES ('A Test Post', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', 2,1230952187,1230952187,1);
 
             INSERT INTO comment (content, status, create_time, author, email, post_id) VALUES ('This is a test comment.', 2, 1230952187, 'Tester', 'tester@example.com', 2);
-
-            INSERT INTO tag (name) VALUES ('yii');
-            INSERT INTO tag (name) VALUES ('blog');
-            INSERT INTO tag (name) VALUES ('test');
 SQL;
 
         Yii::app()->db->createCommand($sql)->execute();
@@ -83,7 +72,9 @@ SQL;
 
 	public function down()
 	{
-		echo "Initial migration does not support migration down.\n";
-		return false;
+        $this->dropTable('lookup');
+        $this->dropTable('comment');
+        $this->dropTable('post');
+        $this->dropTable('user');
 	}
 }
