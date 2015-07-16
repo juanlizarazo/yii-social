3bb47e30796b67f968542bff5f8dc7cac81b1aa0
Juan Lizarazo <gabriellg4@gmail.com>
2015-07-15 20:49:52 -0600

    Layout files, use spaces instead of tabs

--- app/protected/views/layouts/column2.php
+++ app/protected/views/layouts/column2.php
@@ -1,17 +1,17 @@
 <? $this->beginContent('/layouts/main') ?>
-<div class="container-fluid">
-    <div class="col-lg-9">
-        <?= $content ?>
+    <div class="container-fluid">
+        <div class="col-lg-9">
+            <?= $content ?>
+        </div>
+
+        <div class="col-lg-3">
+            <div id="sidebar">
+                <?php if(!Yii::app()->user->isGuest) $this->widget('UserMenu'); ?>
+
+                <?php $this->widget('RecentComments', array(
+                    'maxComments'=>Yii::app()->params['recentCommentCount'],
+                )); ?>
+            </div>
+        </div>
     </div>
-
-	<div class="col-lg-3">
-		<div id="sidebar">
-			<?php if(!Yii::app()->user->isGuest) $this->widget('UserMenu'); ?>
-
-			<?php $this->widget('RecentComments', array(
-				'maxComments'=>Yii::app()->params['recentCommentCount'],
-			)); ?>
-		</div>
-	</div>
-</div>
 <? $this->endContent() ?>
--- app/protected/views/layouts/main.php
+++ app/protected/views/layouts/main.php
@@ -15,69 +15,69 @@
 </head>
 
 <body>
-    <nav class="navbar navbar-default">
-        <div class="container-fluid">
-            <!-- Brand and toggle get grouped for better mobile display -->
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#yii-social-navbar-items" aria-expanded="false">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" href="#"><?= CHtml::encode(Yii::app()->name) ?></a>
-            </div>
-
-            <div id="yii-social-navbar-items" class="collapse navbar-collapse">
-                <ul class="nav navbar-nav">
-                    <?
-                        $this->widget('zii.widgets.CMenu', [
-                            'items' => [
-                                ['label' => 'Home', 'url' => ['post/index']],
-                                ['label' => 'About', 'url' => ['site/page', 'view'=>'about']],
-                                ['label'=>'Contact', 'url' => ['site/contact']]
-                            ],
-                            'activeCssClass' => 'active',
-                            'htmlOptions' => [ 'class' => 'nav navbar-nav' ],
-                        ]);
-                    ?>
-                </ul>
-                <ul class="nav navbar-nav navbar-right">
-                    <?
-                        $this->widget('zii.widgets.CMenu', [
-                            'items' => [
-                                ['label'=>'Login', 'url' => ['site/login'], 'visible' => Yii::app()->user->isGuest],
-                                [
-                                    'label'=>'Logout ('.Yii::app()->user->name.')',
-                                    'url' => ['site/logout'],
-                                    'visible' => !Yii::app()->user->isGuest
-                                ]
-                            ],
-                            'htmlOptions' => ['class' => 'nav navbar-nav']
-                        ]);
-                    ?>
-                </ul>
-            </div>
+<nav class="navbar navbar-default">
+    <div class="container-fluid">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#yii-social-navbar-items" aria-expanded="false">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#"><?= CHtml::encode(Yii::app()->name) ?></a>
         </div>
-    </nav>
 
-    <?
-        $this->widget('zii.widgets.CBreadcrumbs', [
-            'links' => $this->breadcrumbs
-        ])
-    ?>
-
-    <!-- yield to content -->
-    <?= $content ?>
-    <!--/ yield to content-->
-
-    <br>
-    <hr>
-    <footer>
-        <div class="container">
-            Juan Lizarazo &copy; <?= date('Y'); ?>
-            <span class="pull-right"><?= Yii::powered() ?></span>
+        <div id="yii-social-navbar-items" class="collapse navbar-collapse">
+            <ul class="nav navbar-nav">
+                <?
+                $this->widget('zii.widgets.CMenu', [
+                    'items' => [
+                        ['label' => 'Home', 'url' => ['post/index']],
+                        ['label' => 'About', 'url' => ['site/page', 'view'=>'about']],
+                        ['label'=>'Contact', 'url' => ['site/contact']]
+                    ],
+                    'activeCssClass' => 'active',
+                    'htmlOptions' => [ 'class' => 'nav navbar-nav' ],
+                ]);
+                ?>
+            </ul>
+            <ul class="nav navbar-nav navbar-right">
+                <?
+                $this->widget('zii.widgets.CMenu', [
+                    'items' => [
+                        ['label'=>'Login', 'url' => ['site/login'], 'visible' => Yii::app()->user->isGuest],
+                        [
+                            'label'=>'Logout ('.Yii::app()->user->name.')',
+                            'url' => ['site/logout'],
+                            'visible' => !Yii::app()->user->isGuest
+                        ]
+                    ],
+                    'htmlOptions' => ['class' => 'nav navbar-nav']
+                ]);
+                ?>
+            </ul>
         </div>
-    </footer>
+    </div>
+</nav>
+
+<?
+$this->widget('zii.widgets.CBreadcrumbs', [
+    'links' => $this->breadcrumbs
+])
+?>
+
+<!-- yield to content -->
+<?= $content ?>
+<!--/ yield to content-->
+
+<br>
+<hr>
+<footer>
+    <div class="container">
+        Juan Lizarazo &copy; <?= date('Y'); ?>
+        <span class="pull-right"><?= Yii::powered() ?></span>
+    </div>
+</footer>
 </body>
 </html>
