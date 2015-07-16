59f297af034cb694857b148f7e472210bcff8dc3
Juan Lizarazo <gabriellg4@gmail.com>
2015-07-15 20:28:09 -0600

    Update main layout with bootstrap

--- app/protected/config/main.php
+++ app/protected/config/main.php
@@ -7,7 +7,7 @@
 // CWebApplication properties can be configured here.
 return array(
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
-	'name'=>'Yii-social by Juan Lizarazo',
+	'name'=>'Yii-Social',
 
 	// preloading 'log' component
 	'preload'=>array('log'),
--- app/protected/views/layouts/main.php
+++ app/protected/views/layouts/main.php
@@ -1,49 +1,78 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<!DOCTYPE html>
+<html lang="en">
 <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <meta name="language" content="en" />
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="language" content="en">
 
     <!-- Bootstrap -->
     <link rel="stylesheet" href="<?= Yii::app()->request->baseUrl ?>/vendor/twbs/bootstrap/dist/css/bootstrap.min.css">
+    <script type="text/javascript" src="<?= Yii::app()->request->baseUrl ?>/vendor/components/jquery/jquery.js"></script>
     <script type="text/javascript" src="<?= Yii::app()->request->baseUrl ?>/vendor/twbs/bootstrap/dist/js/bootstrap.min.js"></script>
 
-    <title><?php echo CHtml::encode($this->pageTitle); ?></title>
+    <title><?= CHtml::encode($this->pageTitle) ?></title>
 </head>
 
 <body>
+    <nav class="navbar navbar-default">
+        <div class="container-fluid">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#yii-social-navbar-items" aria-expanded="false">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#"><?= CHtml::encode(Yii::app()->name) ?></a>
+            </div>
 
-<div class="container" id="page">
+            <div id="yii-social-navbar-items" class="collapse navbar-collapse">
+                <ul class="nav navbar-nav">
+                    <?
+                        $this->widget('zii.widgets.CMenu', [
+                            'items' => [
+                                ['label' => 'Home', 'url' => ['post/index']],
+                                ['label' => 'About', 'url' => ['site/page', 'view'=>'about']],
+                                ['label'=>'Contact', 'url' => ['site/contact']]
+                            ],
+                            'activeCssClass' => 'active',
+                            'htmlOptions' => [ 'class' => 'nav navbar-nav' ],
+                        ]);
+                    ?>
+                </ul>
+                <ul class="nav navbar-nav navbar-right">
+                    <?
+                        $this->widget('zii.widgets.CMenu', [
+                            'items' => [
+                                ['label'=>'Login', 'url' => ['site/login'], 'visible' => Yii::app()->user->isGuest],
+                                [
+                                    'label'=>'Logout ('.Yii::app()->user->name.')',
+                                    'url' => ['site/logout'],
+                                    'visible' => !Yii::app()->user->isGuest
+                                ]
+                            ],
+                            'htmlOptions' => ['class' => 'nav navbar-nav']
+                        ]);
+                    ?>
+                </ul>
+            </div>
+        </div>
+    </nav>
 
-	<div id="header">
-		<div id="logo"><?php echo CHtml::encode(Yii::app()->name); ?></div>
-	</div><!-- header -->
+    <?
+        $this->widget('zii.widgets.CBreadcrumbs', [
+            'links' => $this->breadcrumbs
+        ])
+    ?>
 
-	<div id="mainmenu">
-		<?php $this->widget('zii.widgets.CMenu',array(
-			'items'=>array(
-				array('label'=>'Home', 'url'=>array('post/index')),
-				array('label'=>'About', 'url'=>array('site/page', 'view'=>'about')),
-				array('label'=>'Contact', 'url'=>array('site/contact')),
-				array('label'=>'Login', 'url'=>array('site/login'), 'visible'=>Yii::app()->user->isGuest),
-				array('label'=>'Logout ('.Yii::app()->user->name.')', 'url'=>array('site/logout'), 'visible'=>!Yii::app()->user->isGuest)
-			),
-		)); ?>
-	</div><!-- mainmenu -->
+    <div class="container-fluid">
+        <?= $content ?>
+    </div>
 
-	<?php $this->widget('zii.widgets.CBreadcrumbs', array(
-		'links'=>$this->breadcrumbs,
-	)); ?><!-- breadcrumbs -->
-
-	<?php echo $content; ?>
-
-	<div id="footer">
-		Copyright &copy; <?php echo date('Y'); ?> by My Company.<br/>
-		All Rights Reserved.<br/>
-		<?php echo Yii::powered(); ?>
-	</div><!-- footer -->
-
-</div><!-- page -->
-
+    <div id="footer">
+        Juan Lizarazo &copy; <?= date('Y'); ?> - <?= Yii::powered() ?>
+    </div>
 </body>
 </html>
