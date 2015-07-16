4744b4a34c2bd4527521346422cb5a1475709ae9
Juan Lizarazo <gabriellg4@gmail.com>
2015-07-16 00:26:11 -0600

    UserIdentity, staces instead of tabs

--- app/protected/components/UserIdentity.php
+++ app/protected/components/UserIdentity.php
@@ -7,34 +7,34 @@
  */
 class UserIdentity extends CUserIdentity
 {
-	private $_id;
+    private $_id;
 
-	/**
-	 * Authenticates a user.
-	 * @return boolean whether authentication succeeds.
-	 */
-	public function authenticate()
-	{
-		$user=User::model()->find('LOWER(username)=?',array(strtolower($this->username)));
-		if($user===null)
-			$this->errorCode=self::ERROR_USERNAME_INVALID;
-		else if(!$user->validatePassword($this->password))
-			$this->errorCode=self::ERROR_PASSWORD_INVALID;
-		else
-		{
-			$this->_id = $user->id;
+    /**
+     * Authenticates a user.
+     * @return boolean whether authentication succeeds.
+     */
+    public function authenticate()
+    {
+        $user=User::model()->find('LOWER(username)=?',array(strtolower($this->username)));
+        if($user===null)
+            $this->errorCode=self::ERROR_USERNAME_INVALID;
+        else if(!$user->validatePassword($this->password))
+            $this->errorCode=self::ERROR_PASSWORD_INVALID;
+        else
+        {
+            $this->_id = $user->id;
             $this->setState('email', $user->email);
             $this->setState('profile_picture', null);
-			$this->errorCode = self::ERROR_NONE;
-		}
-		return $this->errorCode == self::ERROR_NONE;
-	}
+            $this->errorCode = self::ERROR_NONE;
+        }
+        return $this->errorCode == self::ERROR_NONE;
+    }
 
-	/**
-	 * @return integer the ID of the user record
-	 */
-	public function getId()
-	{
-		return $this->_id;
-	}
+    /**
+     * @return integer the ID of the user record
+     */
+    public function getId()
+    {
+        return $this->_id;
+    }
 }
